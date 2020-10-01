f893c756e67536cb29e6e00b49efc8c27318bb0c
Masoud Moradjafari <41879569+justCodeLife@users.noreply.github.com>
2020-10-01 18:37:23 +0330

    Delete 736d9a0b7dbd30057b0316a17f0cf56a86648b29.php

--- Storage/views/736d9a0b7dbd30057b0316a17f0cf56a86648b29.php
+++
@@ -1,7 +0,0 @@
-<?php /* C:\xampp\htdocs\mvcproject\App\Views/errors/404.blade.php */ ?>
-<?php $__env->startSection('content'); ?>
-    <h1>Page not found</h1>
-<?php $__env->stopSection(); ?>
-
-
-<?php echo $__env->make('app', \Illuminate\Support\Arr::except(get_defined_vars(), ['__data', '__path']))->render(); ?>
